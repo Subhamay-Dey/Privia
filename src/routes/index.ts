c54f4e4f928c22249faec26dd646d58be276e5bf
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-02-08 02:35:46 +0530

    fixed all merge conficts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import {Router} from "express";
 import { authRouter } from "../Auth/routes";
-export const router = Router();
-
-router.use("/authentication", authRouter)
 import { rbacRouter } from "../rbac";
 export const router = Router();
 
+router.use("/authentication", authRouter)
 router.use("/rbac", rbacRouter )
 
 //Same for Subhbamay's auth router
