ed6befdcbdaa11807ec3761f1fbac436d13b27b8
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-02-08 00:08:43 +0530

    Removed node_modules, dist, and .env from tracking

--- dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const authRouters_1 = require("./authRouters");
-exports.router = (0, express_1.Router)();
-exports.router.use("/authenticate", authRouters_1.authRouter);
