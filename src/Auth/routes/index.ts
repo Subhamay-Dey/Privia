927c8821305f99e2dea69179c8f1fc37c9602c34
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-08 04:25:05 +0530

    Login Api Controller done and added  validation and error handling ✅

--- src/Auth/routes/index.ts
+++ src/Auth/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import RegisterController from "../Controllers/RegisterController";
+import LoginController from "../Controllers/LoginController";
 
 export const authRouter = Router();
 
 authRouter.post("/register", RegisterController.register);
+
+authRouter.post("/login", LoginController.login);
