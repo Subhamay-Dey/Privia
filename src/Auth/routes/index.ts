cdca0b2ccc7e061a45ab2d437a8daf6f8f7906b6
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-08 17:57:46 +0530

    Added logout route to authRouter with middleware integration ✅

--- src/Auth/routes/index.ts
+++ src/Auth/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import RegisterController from "../Controllers/RegisterController";
 import LoginController from "../Controllers/LoginController";
+import { authMiddleware } from "../middlewares/middleware";
+import LogoutController from "../Controllers/LogoutController";
 
 export const authRouter = Router();
 
 authRouter.post("/register", RegisterController.register);
 
 authRouter.post("/login", LoginController.login);
+
+authRouter.post("/logout", authMiddleware, LogoutController.logout)
